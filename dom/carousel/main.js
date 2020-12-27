03be5a0c384f4a004290d209053fe8fe4f0d82f4
Emiri-i <emiriiskw@gmail.com>
2020-12-28 00:01:39 +0900

    modified

--- dom/carousel/main.js
+++ dom/carousel/main.js
@@ -10,7 +10,7 @@ const images = [
     'images/image006.jpg',
 ];
 
-let currentIndex = 2;
+let currentIndex = 0;
 const mainImage = document.getElementById('carousel__main');
 mainImage.src = images[currentIndex];
 // console.log(mainImage.src);
@@ -26,8 +26,40 @@ for(let [index,image] of images.entries()) {
         li.classList.add('current');
     }
 
+    li.addEventListener('click', () => {
+        mainImage.src = image;
+        mainImage.classList.add('active');
+
+        setTimeout(() => {
+            mainImage.classList.remove('active');
+        },800);
+
+        const thumbnails = document.querySelectorAll('.carousel__thumbnails > li');
+        thumbnails[currentIndex].classList.remove('current');
+        currentIndex = index;
+        thumbnails[currentIndex].classList.add('current');
+    });
+
     li.appendChild(img);
     document.querySelector('.carousel__thumbnails').appendChild(li);
 
 }
+
+const next = document.getElementById('carousel__next');
+next.addEventListener('click', () => {
+    let target = currentIndex + 1;
+    if(target === images.length) {
+        target = 0;
+    }
+    document.querySelectorAll('.carousel__thumbnails > li')[target].click();
+});
+
+const prev = document.getElementById('carousel__prev');
+prev.addEventListener('click', () => {
+    let target = currentIndex - 1;
+    if(target < 0) {
+        target = images.length - 1;
+    }
+    document.querySelectorAll('.carousel__thumbnails > li')[target].click();
+});
 
