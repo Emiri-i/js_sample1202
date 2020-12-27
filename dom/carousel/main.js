9f28874c866206586d99fda639588b049a7aa5ef
Emiri-i <emiriiskw@gmail.com>
2020-12-27 19:39:45 +0900

    added carousel AND tab folder

---
+++ dom/carousel/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const images = [
+    'images/image000.jpg',
+    'images/image001.jpg',
+    'images/image002.jpg',
+    'images/image003.jpg',
+    'images/image004.jpg',
+    'images/image005.jpg',
+    'images/image006.jpg',
+];
+
+let currentIndex = 2;
+const mainImage = document.getElementById('carousel__main');
+mainImage.src = images[currentIndex];
+// console.log(mainImage.src);
+
+for(let [index,image] of images.entries()) {
+    console.log(index,image);
+
+    const img = document.createElement('img');
+    img.src=image;
+
+    const li = document.createElement('li');
+    if (index === currentIndex){
+        li.classList.add('current');
+    }
+
+    li.appendChild(img);
+    document.querySelector('.carousel__thumbnails').appendChild(li);
+
+}
+
