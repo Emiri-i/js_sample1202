a8cb14edb9bc16568b9c9bb3149ec2a5f29871e7
Emiri-i <emiriiskw@gmail.com>
2020-12-31 14:41:06 +0900

    added forms file

--- dom/forms/main.js
+++ dom/forms/main.js
@@ -14,3 +14,17 @@ formTest1yourName.addEventListener('input', () => {
     let yourNameValue = document.getElementById('your_name_value');
     yourNameValue.textContent = formTest1yourName.value.length;
 })
+
+//ファイルのプレビュー表示
+const preview = document.forms.form_test1.preview;
+document.forms.form_test1.image_file.addEventListener('change', (e) => {
+    console.log(e);
+    uploadFile = e.target.files[0];
+
+    //表示 FileReader]
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+        preview.src = reader.result;
+    });
+    reader.readAsDataURL(uploadFile);
+});
